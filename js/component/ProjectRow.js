f217bdb8c085c72782625696cb17395f932b95c0
hmygithub <2713237776@qq.com>
2018-06-10 17:11:04 +0800

    ProjectRow使用onSelect

--- js/component/ProjectRow.js
+++ js/component/ProjectRow.js
@@ -2,24 +2,78 @@
  * Created by lenovo on 2018/6/7.
  */
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, RefreshControl } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-class ProjectRow extends React.Component {
+export default class ProjectRow extends React.Component {
     static defaultProps = {
         item: {}
     }
     render(){
-        const item = this.props.item
+        const item = this.props.item;
         return(
-            <View style={styles.container}>
-                <Text>{item.full_name}</Text>
-            </View>
+            <TouchableOpacity
+                onPress={this.props.onSelect}
+                activeOpacity={0.5}
+                >
+                <View style={styles.container}>
+                    <Text style={styles.title}>{item.full_name}</Text>
+                    <Text style={styles.description}>{item.description}</Text>
+                    <View style={styles.bottom}>
+                        <View style={styles.bottomTextWrapper}>
+                            <Text>作者：</Text>
+                            <Image
+                                style={{width: 22, height: 22}}
+                                source={{uri:item.owner.avatar_url}}/>
+                        </View>
+                        <View style={styles.bottomTextWrapper}>
+                            <Text>星：</Text>
+                            <Text>{item.stargazers_count}</Text>
+                            <Image
+                                style={{width: 22, height: 22}}
+                                source={require("../../res/images/ic_unstar_transparent.png")}/>
+                        </View>
+                    </View>
+                </View>
+            </TouchableOpacity>
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor:'#FFF',
+        flexDirection:'column',
+        padding:10,
+        marginLeft:5,
+        marginRight:5,
+        marginVertical:5,
+        borderColor:'#dddddd',
+        borderWidth:0.5,
+        borderRadius:2,
+        shadowColor:'gray',
+        shadowOffset:{width:0.5,height:0.5},
+        shadowRadius:1, //阴影半径
+        shadowOpacity:0.4,
+        elevation:2 //Android 投影
+    },
+    title:{
+        fontSize:16,
+        marginBottom:2,
+        color:'#212121'
+    },
+    description:{
+        fontSize:14,
+        marginBottom:2,
+        color:'#757575'
+    },
+    bottom:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    bottomTextWrapper:{
+        flexDirection:'row',
+        alignItems:'center'
     }
 })
